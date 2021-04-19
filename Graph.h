0077c1a3178d85c00da74a8980ad0477a01835f6
Steven James <sjames34@siebl-0224-11.ews.illinois.edu>
2021-04-18 23:54:43 -0500

    Set up Graph.h

---
+++ Graph.h
@@ -0,0 +1,44 @@
+/*
+* insertVertex, insertEdge
+* incidentEdges, areAdjacent
+* 
+*/
+
+#pragma once
+
+#include <vector>
+#include <iostream>
+
+
+class Graph {
+    private:
+        struct Vertex {
+            //Integer representing the user's ID
+            int id;
+            int average;
+            Vertex(int id) : id(id) {}
+        };
+
+        struct Edge {
+
+            int rating;
+
+            Edge(int rating) rating(rating) {}
+        };
+        std::vector<Vertex> vertices;
+        std::vector<std::vector<Edge>> matrix; //First dimension is parent vertex, second is child
+
+
+    public:
+        void insertVertex(int id);
+
+        void insertEdge(Vertex first, Vertex second, int weight);
+        
+        std::vector<Edge> incidentEdges(Vertex v);
+
+        bool areAdjacent(Vertex a, Vertex b);
+
+        Edge getEdge(Vertex a, Vertex b);        
+
+        double calculateAverage(Vertex a);
+};
