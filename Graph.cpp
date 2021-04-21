c7144027eecfd25c94f4f7f90ef9f2e6d3ad18d2
Steven James <sjames34@siebl-0224-20.ews.illinois.edu>
2021-04-21 13:57:37 -0500

    Implement insert methods and calculateAverage, change vectors to store pointers

--- Graph.cpp
+++ Graph.cpp
@@ -8,7 +8,7 @@ void Graph::insertVertex(int id) {
 }
 
 void Graph::insertEdge(Vertex first, Vertex second, int weight) {
-    int firstId = first.id - 1;
+    int firstId = first.id - 1; //Indexes are offset by 1
     int secondId = second.id - 1;
 
     Edge* newEdge = new Edge(weight); 
@@ -20,11 +20,11 @@ double Graph::calculateAverage(Vertex a) {
     int sum = 0;
     int total = 0;
 
-    std::vector<Edge> incident = incidentEdges(a);
+    std::vector<Edge*> incidents = incidentEdges(a);
 
-    for (Edge edge : incident) {
+    for (Edge* edge : incidents) {
         total++;
-        sum += edge.weight;
+        sum += edge->weight;
     }
 
     return (sum / total);
--- Graph.h
+++ Graph.h
@@ -30,15 +30,47 @@ class Graph {
 
 
     public:
+        /**
+        * Method that inserts a new vertex (User) into the graph
+        * @param id - The id of the new user
+        */ 
         void insertVertex(int id);
 
+        /**
+        * Method that inserts a new edge that connects two vertices
+        * @param first - The first vertex to connect, this will be the parent
+        * @param second - The second vertex to connect, this will be the child
+        * @param weight - The weight associated with the edge
+        */
         void insertEdge(Vertex first, Vertex second, int weight);
-        
+
+        /**
+        * Method that will get a list of the incident edges of a specific vertex
+        * @param v - The vertex we want to find the incident edges of 
+        * @return a list of edges incident to Vertex v
+        */ 
         std::vector<Edge> incidentEdges(Vertex v);
 
+        /**
+        * Method that will determine if two edges are connected or not
+        * @param a - The first vertex
+        * @param b - The second vertex
+        * @return true if the two edges are adjacent, false otherwise
+        */
         bool areAdjacent(Vertex a, Vertex b);
 
-        Edge getEdge(Vertex a, Vertex b);        
+        /**
+        * A simple method that will return the edge that connects two vertices
+        * @param a - The first vertex
+        * @param b - The second vertex
+        * @return a pointer to the edge that connects the two vertices
+        */
+        Edge* getEdge(Vertex a, Vertex b);        
 
+        /**
+        * Method that will calculate the average rating of a specific user
+        * @param a - The user we want to calculate the average for
+        * @return a double representing the average rating for the user
+        */
         double calculateAverage(Vertex* a);
 };
