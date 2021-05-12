e8c5e7d6654b7babfc459bf0acb7558f53df69c6
Anton Sebastian <asebas6@illinois.edu>
2021-05-11 21:48:52 -0500

    main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,35 @@
-#include <iostream>
+// #include <iostream>
+#include "FileReader.h"
+#include "Graph.h"
 
 int main (int argc, const char * argv[]) {
-    std::cout << "Hello World";
+
+    Graph graph(7604);
+
+    std::ifstream in; 
+    in.open(argv[1]);
+  
+    std::string output;
+    while(!in.eof()) {
+        int rater, ratee, rating; 
+
+        getline(in, output, ',');
+        rater = stoi(output);
+
+        getline(in, output, ',');
+        ratee = stoi(output);
+
+        getline(in, output, ',');
+        rating = stoi(output);
+
+        getline(in, output, '\n');
+
+        //graph.insertEdge(rater, ratee, rating);
+        
+        std::cout << "rater: " << rater << " ratee: " << ratee << " rating: " << rating << std::endl;
+    }
+
+    
+
+
 }
